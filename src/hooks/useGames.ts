81d0b758c6fb1d60d4082bd07cf05e42f64ad794
anthony-iheonye <iheonyeac@gmail.com>
2024-11-05 02:46:25 -0500

    Fetch game details

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -9,6 +9,7 @@ const apiClient = new APIClient<Game>("/games");
 export interface Game {
   id: number;
   slug: string;
+  description_raw: string;
   name: string;
   background_image: string;
   parent_platforms: { platform: Platform }[];
