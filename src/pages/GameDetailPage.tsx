474e2dde0f04aa26a98391cf425b066c8914645d
anthony-iheonye <iheonyeac@gmail.com>
2024-11-05 20:38:29 -0500

    Add copyright page

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,5 +1,6 @@
 import { Box, Heading, SimpleGrid, Spinner } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
+import CopyrightBar from "../components/CopyrightBar";
 import ExpandableText from "../components/ExpandableText";
 import GameAttributes from "../components/GameAttributes";
 import GameScreenShot from "../components/GameScreenShot";
@@ -15,17 +16,20 @@ const GameDetailPage = () => {
   if (error || !game) throw error;
 
   return (
-    <SimpleGrid columns={{ base: 1, md: 2 }} spacing={5}>
-      <Box>
-        <Heading>{game.name}</Heading>
-        <ExpandableText>{game.description_raw}</ExpandableText>
-        <GameAttributes game={game} />
-      </Box>
-      <Box>
-        <GameTrailer gameId={game.id} />
-        <GameScreenShot gameId={game.id} />
-      </Box>
-    </SimpleGrid>
+    <>
+      <SimpleGrid columns={{ base: 1, md: 2 }} spacing={5}>
+        <Box>
+          <Heading>{game.name}</Heading>
+          <ExpandableText>{game.description_raw}</ExpandableText>
+          <GameAttributes game={game} />
+        </Box>
+        <Box>
+          <GameTrailer gameId={game.id} />
+          <GameScreenShot gameId={game.id} />
+        </Box>
+      </SimpleGrid>
+      <CopyrightBar />
+    </>
   );
 };
 
