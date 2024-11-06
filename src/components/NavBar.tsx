fcc82a37c680b23323c8d5248ac472dde44877f0
anthony-iheonye <iheonyeac@gmail.com>
2024-11-05 23:14:18 -0500

    Fix the NavBar - refresh app upon clicking icon.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,11 +3,14 @@ import { Link } from "react-router-dom";
 import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
+import useGameQueryStore from "../store";
 
 const NavBar = () => {
+  const setSearchText = useGameQueryStore((s) => s.setSearchText);
+
   return (
     <HStack padding="10px">
-      <Link to="/">
+      <Link to="/" onClick={() => setSearchText("")}>
         <Image src={logo} boxSize="60px" objectFit="cover" />
       </Link>
 
